a3391eed10ba42d251923e4ba1c34dbbcb205375
simonhuwiler <webmaster@simonhuwiler.ch>
2021-04-15 18:43:10 +0200

    wip

---
+++ python/cleandata.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+def clean(df):
+    # Transform data
+    df.loc[df.partei_nicht_zugeordnet == 1, 'partei_nicht_zugeordnet'] = True
+    df['partei_nicht_zugeordnet'] = df['partei_nicht_zugeordnet'].fillna(False)
+
+    df[df.jahrgang_nicht_zugeordnet == 1] = True
+    df['jahrgang_nicht_zugeordnet'] = df['jahrgang_nicht_zugeordnet'].fillna(False)
+
+    # Alter berechnen
+    df['Alter'] = 2021 - df['Jahrgang']
+
+    print("Jahrgänge nicht zugeordnet: %s" % len(df[df.jahrgang_nicht_zugeordnet == True]))
+    print("Partei nicht zugeordnet: %s" % len(df[df.partei_nicht_zugeordnet == True]))
+    print("Keine Jahrgänge: %s" % len(df[df.Jahrgang.isna()]))
+    
+    # Clean Party
+    
+    # Add partei_clean
+    def transform_partei(p):
+
+        if type(p) == str:
+            p = p.lower()
+
+        if p == 'die mitte': p = 'cvp'
+
+        if p in ['parteilos', 'fdp', 'evp', 'svp', 'sp', 'cvp', 'glp', 'grüne', 'bdp', 'al', 'edu']:
+            return p
+        return 'verschiedene'
+
+    df['partei_c'] = df['Partei'].apply(transform_partei)
+    
+    return df
