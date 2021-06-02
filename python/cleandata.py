ad5f5f076b2b5fca71c87e00834b725b023582ef
simonhuwiler <webmaster@simonhuwiler.ch>
2021-06-02 16:28:27 +0200

    wip

--- python/cleandata.py
+++ python/cleandata.py
@@ -33,3 +33,48 @@ def clean(df):
     df.loc[df.Name.str.lower() == 'vakant', 'partei_c'] = 'vacant'
     
     return df
+
+def stadt_land(df):
+    # Define which ones are cities (not Gemeinden)
+    stadtrat = ['Adliswil', 'Affoltern am Albis', 'Bülach', 'Dietikon', 'Dübendorf', 'Illnau-Effretikon', 'Kloten', 'Opfikon', 'Schlieren', 'Uster', 'Wädenswil', 'Wetzikon', 'Winterthur', 'Zürich']
+
+    df['type'] = 'land'
+    df.loc[df.Gemeinde.isin(stadtrat), 'type'] = 'stadt'
+    return df
+
+def add_bfs(df):
+    df_bfs = pd.read_csv('../data/bfs_name.csv', sep=';')
+
+    df_bfs.rename(columns={'BFS': 'bfs'}, inplace=True)
+
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.strip()
+
+    # Clean Names
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('a.A.', 'am Albis', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('a.I.', 'am Irchel', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('a.d.Th.', 'an der Thur', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('a.S.', 'am See', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('a.d.L.', 'an der Limmat', regex=False)
+
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Zell', 'Zell (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Schlatt', 'Schlatt (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Rüti', 'Rüti (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Wald', 'Wald (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Wetzikon', 'Wetzikon (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Kilchberg', 'Kilchberg (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Erlenbach', 'Erlenbach (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Gossau', 'Gossau (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Rickenbach', 'Rickenbach (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Weiningen', 'Weiningen (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Birmensdorf', 'Birmensdorf (ZH)', regex=False)
+    df_bfs.loc[df_bfs.Gemeinden == 'Wil', 'Gemeinden'] = 'Wil (ZH)'
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Buchs', 'Buchs (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Benken', 'Benken (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Küsnacht', 'Küsnacht (ZH)', regex=False)
+    df_bfs['Gemeinden'] = df_bfs['Gemeinden'].str.replace('Aesch', 'Aesch (ZH)', regex=False)
+
+    df = df.merge(df_bfs, how='left', left_on = 'Gemeinde', right_on='Gemeinden')
+    df.drop(columns='Gemeinden', inplace=True)
+
+    return df
+
